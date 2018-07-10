af5d0126d3a65c664ba661195df3e9caa3431cfd
Bernardo Campos <bcsfreitas@gmail.com>
2018-07-10 20:47:47 -0300

    Inicio de pagina inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,64 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'app';
+  tituloProduto = 'Acadêmico Total';
+
+  features: any = [
+    {
+      nome: 'Ocorrencias',
+      descricao: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
+      icone: ''
+    },
+    {
+      nome: 'Boletim',
+      descricao: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
+      icone: ''
+    },
+    {
+      nome: 'Controle de Acesso',
+      descricao: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
+      icone: ''
+    },
+  ];
+
+  beneficios: any = [
+    {
+      nome: 'Redução do uso de recursos',
+      beneficios: [
+        '-80% de Papel',
+        'Tempo de Pessoal',
+        'Consumo de Energia',
+        'Consumo de Toner',
+        'Conta de Telefone',
+        'Fácil integração com sustemas de gestão atuais da escola'
+      ]
+    },
+    {
+      nome: 'Controle Disciplinar',
+      beneficios: [
+        'Redução de evasão escolar',
+        'Aumento da eficiência do conselho de Classe',
+        'Auxílio do Controle de ocorrências na sala de aula'
+      ]
+    },
+    {
+      nome: 'Mais Participação de Responsáveis',
+      beneficios: [
+        'Comunicados e Ocorrências com monitoramento de quem leu',
+        'Controle em qualquer lugar através de App',
+        'Aumento de 80% de responsáveis nas reuniões',
+        'Suporte para pais separados / divorciados'
+      ]
+    },
+  ];
+
+  contagem: any = {
+    alunos: '8.763',
+    educadores: '273',
+    cidades: '13',
+    escolas: '26'
+  };
 }
